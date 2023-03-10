847e22af8f36802d72ce45f4b6dfd659eb7275e1
[Alexandr] <[afominyh242@gmail.com]>
2023-03-11 01:41:54 +0700

    Добавил возможность смотерть сколько времени находится клиент

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+\
+,.idea/inspectionProfiles/Project_Default.xml,4/9/496a238a6afa168dbaf6efd37bb459331589579c
+^
+..idea/inspectionProfiles/profiles_settings.xml,1/e/1e9075f5bf079c01ef2c910709e91a497d262080
+;
+poetry.lock,0/0/0068a1506b061a8948c37d78b0dd5697211558d3
+>
+pyproject.toml,5/d/5d07e7d72637aa0d59c89d381fe6dc4cf46e2491
+@
+requirements.txt,1/9/19359a61ae2446b51b549167b014da2fcf265768
--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import os
 
 import django
+from django.utils import timezone
+import datetime as dt
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Visit  # noqa: E402
 
 if __name__ == '__main__':
-    # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+
+    visitor_in_vault = Visit.objects.filter(leaved_at__isnull=True)
+    visitor_entered_local_time = django.utils.timezone.localtime(visitor_in_vault[0].entered_at)
+    visitor_entered_utc_time = visitor_in_vault[0].entered_at
+    now_time = dt.datetime.utcnow()
+    delta_time = visitor_entered_utc_time - now_time
+
+
+    print(f'Зашёл в хранилище, время по Москве: {os.linesep}'
+          f'{visitor_entered_local_time}'
+          f'{os.linesep}'
+          f'Находится в хранилище: {os.linesep}'
+          f'{visitor_entered_utc_time} {visitor_entered_local_time} {now_time} {delta_time}')
+
+    # a = datetime.date("%Y/%m/%d-%H.%M.%S")
+    # print(a)
